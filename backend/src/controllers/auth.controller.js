c6420b42231793989647817720a4774be623fd9a
Himanshu Mishra <himanshumishra389@gmail.com>
2025-04-27 00:04:22 +0530

    first commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,45 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/util.js";
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // Create new user
+        const newUser = new User({
+            fullname: fullName,
+            email,
+            password: hashedPassword,
+        });
+        generateToken(newUser._id,res)
+        await newUser.save();
+        // Generate JWT token
+       
+        res.status(201).json({
+            message: "User created successfully",
+            user: {
+                id: newUser._id,
+                fullname: newUser.fullname,
+                email: newUser.email,
+            },
+            token,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong", error: error.message });
+    }
+};
+
+export const login=(req,res)=>{
+    res.send("login route");
+}
+export const logout=(req,res)=>{
+    res.send("logout route")
+}
