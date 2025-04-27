db86fbdb29d36df111174a864ae5c04db0a59aba
Himanshu Mishra <himanshumishra389@gmail.com>
2025-04-28 02:31:25 +0530

    all  https apis are ready except messaing

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { generateToken } from "../lib/util.js";
-
+import cloudinary from "../lib/cloudinary.js";
 export const signup = async (req, res) => {
     // console.log(req.body)
     const { fullName, email, password } = req.body;
@@ -23,7 +23,7 @@ export const signup = async (req, res) => {
         const token = generateToken(newUser._id, res);
         await newUser.save();
         // Generate JWT token
-       
+
         res.status(201).json({
             message: "User created successfully",
             user: {
@@ -37,12 +37,59 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Something went wrong", error: error.message });
     }
 };
-export const testup=(req,res)=>{
-    res.send("login route");
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' })
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Inavalid credentials" });
+        }
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error is login controller", error.message)
+    }
+
 }
-export const login=(req,res)=>{
-    res.send("login route");
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Logout failed", error: error.message });
+    }
 }
-export const logout=(req,res)=>{
-    res.send("logout route")
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body
+        const userId = req.user._id
+        if (!profilePic) {
+            return res.status(400).json({ message: "Profile pic is required" })
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updateUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+        res.status(200).json(updateUser)
+    } catch (error) {
+        res.status(500).json({ message: "Failed to update profile", error: error.message });
+    }
+}
+
+export const checkAuth = async (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.nessage)
+        res.statuus(500).json({ message: "Internal Server Error" })
+    }
 }
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, logout, signup, testup } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile, } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 const router=express.Router();
 
 router.post("/signup",signup)
-router.get("/login",login)
-router.get("/logout",logout)
-
-
+router.post("/login",login)
+router.post("/logout",logout)
+router.put('/update-profile',protectRoute,updateProfile)
+router.post("/check",protectRoute,checkAuth)
 export default router;
